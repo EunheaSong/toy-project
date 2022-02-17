8a3ab5ed799e6d4a25f23cd3574ff193369d08c5
Eunhye Song <dmsgp8292@gmail.com>
2022-02-18 00:18:45 +0900

    test

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def test_get():
 def test_post():
    title_receive = request.form['title_give']
    print(title_receive)
-   return jsonify({'result':'success', 'msg': '이 요청은 POST!'})
+   return jsonify({'result':'success', 'msg': '!'})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
