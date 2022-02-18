5b7e96cfaf533045ce0137b54c90d522032883c8
Eunhye Song <dmsgp8292@gmail.com>
2022-02-19 01:38:13 +0900

    2/18까지 정리 commit

--- app.py
+++ app.py
@@ -17,7 +17,51 @@ db = client.toy   #db폴더명 toy로 설정.
 
 @app.route('/')
 def home():
+    return render_template('index.html')
+
+@app.route('/offline') #onclick 시, offline.html로 이동하고 싶을 때,  location.href='/offline' 이라고 하면된다.
+def offline_page():
+    return render_template('offline.html')
+
+@app.route('/writeForm') # writeForm.html로 이동 할 때
+def writeForm_page():
+    return render_template('writeForm.html')
+
+@app.route('/study_page_offline')  # study_page_offline.html 로 이동 할 때
+def post_page():
+    return render_template('study_page_offline.html')
+
+@app.route('/study_page_online')  # study_page_online.html 로 이동 할 때
+def post_page():
+    return render_template('study_page_online.html')
+
+@app.route('/postbox')  # postbox.html 로 이동 할 때
+def post_page():
     return render_template('postbox.html')
+
+########## 권규민 - 게시판 페이지 작성 후 DB에 insert. ###############
+
+@app.route('/write', methods=["POST"] )
+def write():
+
+    #게시글 넘버링 - 해당 번호로 내용을 가져오며, 삭제,수정의 index 번호역활.
+    Write_list = list(db.posts.find({}, {"_id": False}))
+    count = len(Write_list) + 1
+
+    name_receive = request.form['name_give']
+    title_receive = request.form['title_give']
+    content_receive = request.form['content_give']
+
+    doc = {
+        'num' : count,
+        'name': name_receive,
+        'title': title_receive,
+        'content': content_receive,
+        'hit': 0
+    }
+    db.posts.insert_one(doc)
+
+    return jsonify({'msg': '작성완료!'})
 
 
 ########## 이푸름 - 전체보기/온라인/오프라인 게시글 목록 페이지 ##########
@@ -27,19 +71,12 @@ def study_page_get():
    post_list = list(db.posts.find({},{'_id':False}))
    return jsonify({'post': post_list})
 
-##################################################
-
 
 ########## 송은혜 - 스터디 게시글 내용 보는 페이지 ##########
 
 @app.route('/study_page') #study_page.html로 페이지 이동. url명을 지정해주고자 하여,study_page로 설정.
 def study_page():
     return render_template('study_page.html')
-
-@app.route("/postbox", methods=["GET"]) #게시글 내용 꺼내옴
-def posts_get():
-    all_posts = list(db.posts.find({}, {'_id': False}))
-    return jsonify({'posts':all_posts})
 
 @app.route("/postbox/comment", methods=["GET"]) #게시글 내용 꺼내옴
 def comments_get():
@@ -64,31 +101,6 @@ def comment_delete():
 
 
 ##################################################
-
-#권규민 - 게시판 페이지 작성 후 DB에 insert.
-
-@app.route('/write', methods=["POST"] )
-def write():
-
-    #게시글 넘버링 - 해당 번호로 내용을 가져오며, 삭제,수정의 index 번호역활.
-    Write_list = list(db.posts.find({}, {"_id": False}))
-    count = len(Write_list) + 1
-
-    name_receive = request.form['name_give']
-    title_receive = request.form['title_give']
-    content_receive = request.form['content_give']
-
-    doc = {
-        'num' : count,
-        'name': name_receive,
-        'title': title_receive,
-        'content': content_receive,
-        'hit': 0
-    }
-    db.posts.insert_one(doc)
-
-    return jsonify({'msg': '작성완료!'})
-
 
 
 
